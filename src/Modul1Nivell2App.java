506d3658f22480d5844a2911b4b035240a68bf65
Lluis Gutierrez <betta_betta@hotmail.com>
2021-02-26 16:09:36 +0100

    ModulNivell2

---
+++ src/Modul1Nivell2App.java
@@ -0,0 +1,21 @@
+
+public class Modul1Nivell2App {
+
+	public static void main(String[] args) {
+		double numero1=4.3456;
+		System.out.println("El número double és: "+ numero1);
+		int numero2=(int)numero1;
+		System.out.println("El número double " + numero1 + " és " + numero2 + " en int");
+		float numero3=(float)numero1;
+		System.out.println("El número double " + numero1 + " és " + numero3 + " en float");
+		String texte = String.valueOf(numero1);
+		System.out.println("El número double " + numero1 + " és " + texte + " en string");
+		
+		
+		
+		
+		
+		
+	}
+
+}
